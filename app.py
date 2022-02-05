37ceeb855c2e3729dd3470277d40a2c7202bfd9a
holly.inlet <85998228+holly-inlet@users.noreply.github.com>
2022-02-05 20:36:30 +0900

    デバックモード解除

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, date
 
 
+debug = False
+
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
@@ -70,4 +73,4 @@ def delete(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=debug)

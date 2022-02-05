e7824498a781f99d8bfc79965116586216fd7e39
holly.inlet <85998228+holly-inlet@users.noreply.github.com>
2022-02-05 18:27:34 +0900

    Todoリストアプリ制作チュートリアル
    (1)テックダイアリー記事のSTEP12を実施

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date
 
 
 app = Flask(__name__)
@@ -19,7 +19,7 @@ class Post(db.Model):
 def index():
     if request.method == "GET":
         posts = Post.query.order_by(Post.due).all()
-        return render_template("index.html", posts=posts)
+        return render_template("index.html", posts=posts, today=date.today())
     
     else:
         title = request.form.get("title")

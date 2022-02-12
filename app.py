749d64c814be67943ff74316b7a223f4fa4be7ae
holly.inlet <85998228+holly-inlet@users.noreply.github.com>
2022-02-12 16:09:10 +0900

    add:経過時間計算処理

--- app.py
+++ app.py
@@ -1,45 +1,63 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, date
+
+from datetime import datetime
 
 
-DEBUG = False
+DEBUG = True
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///todo.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///action-log.db'
 db = SQLAlchemy(app)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), nullable=False)
-    detail = db.Column(db.String(100))
-    due = db.Column(db.DateTime, nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    task = db.Column(db.String(30), nullable=False)
+    tag = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Time)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        posts = Post.query.order_by(Post.due).all()
-        return render_template("index.html", posts=posts, today=date.today())
+        posts = Post.query.order_by(Post.start_time).all()
+        return render_template("index.html", posts=posts, today=datetime.today())
     
     else:
-        title = request.form.get("title")
-        detail = request.form.get("detail")
-        due = request.form.get("due")
+        task = request.form.get("task")
+        tag = request.form.get("tag")
+        date = request.form.get("date")
+        time = request.form.get("time")
+        duration = None
         
-        due = datetime.strptime(due, "%Y-%m-%d")
-        new_post = Post(title=title, detail=detail, due=due)
+        start_time = datetime.strptime(date + ' ' + time, "%Y-%m-%d %H:%M")
         
+        before_task = Post.query.order_by(Post.start_time.desc()).filter(
+            Post.start_time < start_time).first()
+        if before_task:
+            before_time = start_time - before_task.start_time
+            before_task.duration = datetime.time(
+                datetime.strptime(str(before_time), "%H:%M:%S"))
+        
+        next_task = Post.query.order_by(Post.start_time).filter(
+            Post.start_time > start_time).first()
+        if next_task:
+            duration = next_task.start_time - start_time
+            duration = datetime.time(
+                datetime.strptime(str(duration), "%H:%M:%S"))
+        
+        new_post = Post(
+            task=task, 
+            tag=tag, 
+            start_time=start_time, 
+            duration=duration
+            )
         db.session.add(new_post)
         db.session.commit()
         return redirect("/")
-
-
-@app.route("/create")
-def create():
-    return render_template("create.html")
 
 
 @app.route("/update/<int:id>", methods=["GET", "POST"])
@@ -49,18 +67,22 @@ def update(id):
         return render_template("update.html", post=post)
     
     else:
-        post.title = request.form.get("title")
-        post.detail = request.form.get("detail")
-        post.due = datetime.strptime(request.form.get("due"), "%Y-%m-%d")
+        date = request.form.get("date")
+        time = request.form.get("time")
+        start_time = datetime.strptime(date + ' ' + time, "%Y-%m-%d %H:%M")
+        
+        post.task = request.form.get("task")
+        post.tag = request.form.get("tag")
+        post.start_time = start_time
         
         db.session.commit()
         return redirect("/")
 
 
-@app.route("/detail/<int:id>")
+@app.route("/tag/<int:id>")
 def read(id):
     post = Post.query.get(id)
-    return render_template('detail.html', post=post)
+    return render_template('tag.html', post=post)
 
 
 @app.route("/delete/<int:id>")
